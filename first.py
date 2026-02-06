81649fc505477cac03478ada54190b91ac63f76d
rileyperera <350179669@gapps.yrdsb.ca>
2026-02-06 11:47:56 -0500

    I did something

--- first.py
+++ first.py
@@ -1,1 +1,37 @@
-print("Hello, world!")
+# First actual program.
+# variables cannot be started with a number, they are often started with a lowercase letter. 
+# variables are dynamic and can be updated 
+# Datatype of variable doesn't need to be specified. 
+# Python doesn't need an ending like a semicolon.
+# the equals sign represents the assignment operator (variable on the left and value on the right)
+# right side of equal sign is the value of the data
+# int = integer 
+# float = +/- decimal 
+# bool = True or False
+# str = String, alpha, numeric, special symbols 
+# list = datatype which can hold many other data types
+# dict = dictionary 
+# input is an example of a built in function
+# the input function stores the string input from the terminal to the related variable 
+# end is a built in function for strings to end them without a space, end is assumed as \n (new line)
+# sep is what seperates the two strings, sep is assumed as space
+
+# message = "Hello, welcome to ICS4U"  
+# name = input("What is your name?: ")
+# print(message, name, end=" ", sep=" ")
+# print("HELLO BUDDY")
+
+#if we add two ints then it literally adds them
+# command + / makes everything comments
+
+# num1 = 2
+# num2 = 1
+# print(num1 + num2)
+
+#input function reads everything as a string 
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print(num1 + num2)
+
+# int() is a composite of both so in this case it takes the strings and converts them to integers which is why we get 3 now instead of 21
